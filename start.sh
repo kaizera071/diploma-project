26b6bb06b06c128289f22c6afbd7fad5290b6e61
= <=>
2024-07-08 19:30:20 +0300

    ingress setup

--- start.sh
+++ start.sh
@@ -46,23 +46,51 @@ build_docker_image() {
 # Function to deploy Docker image in Minikube
 deploy_to_minikube() {
     local deployment_file="$1"
-    local service_name="$2"
+    local service_file="$2"
 
     echo "Deploying to Minikube..."
     kubectl apply -f "$deployment_file"
-
-    kubectl apply -f "audit-system-parent/ingestion/k8s/service.yaml"
-
-    #echo "Exposing service..."
-    #kubectl expose deployment "$service_name" --type=NodePort --port=80
+    kubectl apply -f "$service_file"
 }
 
-# Function to get service URL
-get_service_url() {
-    local service_name="$1"
+wait_for_pod_running() {
+    local POD_NAME=$1
+    echo "Waiting for pod $POD_NAME to be running..."
+    while true; do
+        POD_STATUS=$(kubectl get pod $POD_NAME -o jsonpath='{.status.phase}')
+        if [[ $POD_STATUS == "Running" ]]; then
+            echo "Pod $POD_NAME is now running."
+            break
+        elif [[ $POD_STATUS == "Pending" ]]; then
+            echo "Pod $POD_NAME is still pending. Waiting..."
+        else
+            echo "Pod $POD_NAME is in state: $POD_STATUS"
+            exit 1
+        fi
+        sleep 3  # Adjust the interval as needed
+    done
+}
 
-    echo "Service URL:"
-    minikube service "$service_name" --url
+apply_resource_with_retry() {
+    local resource_yaml=$1  # Path to Resource YAML file passed as argument
+    local retries=3         # Number of retry attempts
+    local delay=3          # Delay between retries in seconds
+
+    for (( i=1; i<=$retries; i++ )); do
+        echo "Attempt $i: Applying Resource from $resource_yaml..."
+        kubectl apply -f "$resource_yaml"
+
+        if [ $? -eq 0 ]; then
+            echo "Resource applied successfully."
+            return 0
+        else
+            echo "Resource application failed. Retrying in $delay seconds..."
+            sleep $delay
+        fi
+    done
+
+    echo "Failed to apply Resource after $retries attempts."
+    return 1
 }
 
 # Start Minikube if not already running
@@ -81,7 +109,15 @@ build_ingestion_service
 build_docker_image "ingestion:07.07.2024" audit-system-parent/ingestion/k8s/Dockerfile
 
 # Deploy to Minikube
-deploy_to_minikube "audit-system-parent/ingestion/k8s/deployment.yaml" "ingestion"
+deploy_to_minikube "audit-system-parent/ingestion/k8s/deployment.yaml" "audit-system-parent/ingestion/k8s/service.yaml"
+apply_resource_with_retry "audit-system-parent/ingress.yaml"
 
-# Get service URL
-get_service_url "ingestion"
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm install my-nginx-ingress ingress-nginx/ingress-nginx --set controller.extraLabels.app=nginx-ingress
+
+POD_NAME=$(kubectl get pods -l app.kubernetes.io/instance=my-nginx-ingress -o jsonpath='{.items[0].metadata.name}')
+
+# Wait for the pod to become running
+wait_for_pod_running "$POD_NAME"
+
+kubectl port-forward $POD_NAME 8080:80
