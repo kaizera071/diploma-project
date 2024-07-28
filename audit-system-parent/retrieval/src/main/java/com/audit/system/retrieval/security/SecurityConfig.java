957314eb3529f9189598698c52e519c41efc234b
= <=>
2024-07-28 17:47:27 +0300

    refactor

--- audit-system-parent/retrieval/src/main/java/com/audit/system/retrieval/security/SecurityConfig.java
+++ audit-system-parent/retrieval/src/main/java/com/audit/system/retrieval/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @SuppressWarnings("removal")
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
