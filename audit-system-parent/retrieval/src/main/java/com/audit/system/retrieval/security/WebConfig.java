036ea423f446d108e75f047711717d66682b42fd
Miroslav Tsvetanov <tsvetanov071@gmail.com>
2024-08-10 12:13:02 +0300

    add webConfig

---
+++ audit-system-parent/retrieval/src/main/java/com/audit/system/retrieval/security/WebConfig.java
@@ -0,0 +1,18 @@
+package com.audit.system.retrieval.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://kaizera071.github.io") // Replace with your frontend domain
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true);
+    }
+}
